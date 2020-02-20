5accf8746d28a7a97297142c0e53dbdd08b8792b
Haitam <riadihaitam@gmail.com>
2020-02-20 21:52:47 +0100

    first commit

--- lib/providerPasserCommande/passer_commande.dart
+++ lib/providerPasserCommande/passer_commande.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
 import 'package:carte_app/providerLogin/model/user_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:csv/csv.dart';
 
 class PasserCommande extends StatefulWidget {
   @override
@@ -11,6 +16,42 @@ class PasserCommande extends StatefulWidget {
 }
 
 class PasserCommandeState extends State<PasserCommande> {
+  File  pickedImage;
+  List<List<String>> textList=[];
+  List<String> lineList=[];
+
+  bool isImageLoaded = false;
+  String text = "";
+  Future pickImage() async {
+    var tempStore = await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      pickedImage = tempStore;
+      isImageLoaded = true;
+    });
+  }
+
+  Future readText() async {
+
+    textList = [];
+    FirebaseVisionImage ourImage = FirebaseVisionImage.fromFile(pickedImage);print('2');
+    TextRecognizer recognizeText = FirebaseVision.instance.textRecognizer();
+    VisionText readText = await recognizeText.processImage(ourImage);
+    for (TextBlock block in readText.blocks) {
+      for (TextLine line in block.lines) {
+        for (TextElement word in line.elements) {
+          lineList.add(word.text);
+        }
+        textList.add(lineList);
+        lineList = [];
+      }
+    }
+
+    String csv = const ListToCsvConverter().convert(textList);
+    print(csv);
+    setState(() {
+      text=text+csv;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +78,32 @@ class PasserCommandeState extends State<PasserCommande> {
           ),
           backgroundColor: Color(0xFFE65100),
         ),
-        body: Text('Passer commande')
+        body: Column(
+          children: <Widget>[
+            SizedBox(height: 100.0),
+            isImageLoaded
+                ? Center(
+              child: Container(
+                  height: 200.0,
+                  width: 200.0,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: FileImage(pickedImage), fit: BoxFit.cover))),
+            )
+                : Container(),
+            SizedBox(height: 10.0),
+            RaisedButton(
+              child: Text('Pick an image'),
+              onPressed: pickImage,
+            ),
+            SizedBox(height: 10.0),
+            RaisedButton(
+              child: Text('Read Text'),
+              onPressed: readText,
+            ),
+            Text(text)
+          ],
+        )
     );
   }
 
